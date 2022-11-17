4fb2952982e628f296bd5484ae295a2354e89198
Darian Southerland <darsouthland@gmail.com>
2022-11-17 13:58:28 -0600

    Upd1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         React is so cool!
         </p>
         <a
           className="App-link"
@@ -18,6 +18,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>
+        <p></p>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </h1>
     </div>
   );
 }
